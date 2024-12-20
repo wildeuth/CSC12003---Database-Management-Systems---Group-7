e5ffffc2f5cb8c99e8390e938b2b4777d74a844c
Lyan <22127344@student.hcmus.edu.vn>
2024-12-20 14:12:36 +0700

    Cai co lap

--- bo phan kinh doanh/ThongKeKhachHang.sql
+++ bo phan kinh doanh/ThongKeKhachHang.sql
@@ -5,28 +5,36 @@ CREATE OR ALTER PROCEDURE sp_ThongKeKhachHangHangNgay
 AS
 BEGIN
     SET NOCOUNT ON;
-
+	SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
 	
-    -- Bước 1: Lấy số lượng khách hàng của từng sản phẩm
-    Create table #TableSoLuongKhachHang (MaSanPham INT, SoLuongKhachHang INT);
-    INSERT INTO #TableSoLuongKhachHang (MaSanPham, SoLuongKhachHang)
-    EXEC sp_LaySoLuongKhachHangMuaSanPham @Ngay;
+	begin transaction
+	begin try
+		-- Bước 1: Lấy số lượng khách hàng của từng sản phẩm
+		Create table #TableSoLuongKhachHang (MaSanPham INT, SoLuongKhachHang INT);
+		INSERT INTO #TableSoLuongKhachHang (MaSanPham, SoLuongKhachHang)
+		EXEC sp_LaySoLuongKhachHangMuaSanPham @Ngay;
 
-    -- Bước 2: Tính tổng số lượng khách hàng
-    SELECT @Tong = SUM(SoLuongKhachHang)
-    FROM #TableSoLuongKhachHang;
+		-- Bước 2: Tính tổng số lượng khách hàng
+		SELECT @Tong = SUM(SoLuongKhachHang)
+		FROM #TableSoLuongKhachHang;
 
-    -- Bước 3: Trả kết quả
-    SELECT @Tong AS TongSoLuongKhachHang;
+		-- Bước 3: Trả kết quả
+		SELECT @Tong AS TongSoLuongKhachHang;
 
-	drop table #TableSoLuongKhachHang
+		drop table #TableSoLuongKhachHang
+
+		COMMIT TRANSACTION
+	END TRY
+    BEGIN CATCH
+        ROLLBACK TRANSACTION;
+        THROW;
+    END CATCH;
+
 END
 GO
 
 DECLARE @TongKhachHang INT;
 EXEC sp_ThongKeKhachHangHangNgay @Ngay =  '2024-11-01 14:00:00.000', @Tong = @TongKhachHang OUTPUT;
-
-PRINT 'Tổng số lượng khách hàng: ' + CAST(@TongKhachHang AS NVARCHAR);
 
 -- Lấy số lượng khách hàng đã mua sản phẩm
 CREATE or alter PROCEDURE sp_LaySoLuongKhachHangMuaSanPham
@@ -34,28 +42,40 @@ CREATE or alter PROCEDURE sp_LaySoLuongKhachHangMuaSanPham
 AS
 BEGIN
     SET NOCOUNT ON;
-	CREATE TABLE #DanhSachSanPham (MaSanPham INT);
+	SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
 
-    -- Lấy danh sách sản phẩm đã bán
-    INSERT INTO #DanhSachSanPham (MaSanPham)
-    SELECT DISTINCT CTHD.MaSanPham
-    FROM CHI_TIET_HOA_DON AS CTHD WITH (READCOMMITTED, ROWLOCK)
-    INNER JOIN HOA_DON AS HD
-        ON CTHD.MaHoaDon = HD.MaHoaDon
-    INNER JOIN PHIEU_MUA_SAM AS PMS
-        ON HD.MaPhieuMuaSam = PMS.MaPhieuMuaSam
-    WHERE PMS.NgayDat = @Ngay;
+	BEGIN TRANSACTION;
+    BEGIN TRY
 
-    -- Đếm số lượng khách hàng của từng sản phẩm
-    SELECT SP.MaSanPham, COUNT(DISTINCT PMS.MaKhachHang) AS SoLuongKhachHang
-    FROM PHIEU_MUA_SAM AS PMS WITH (READCOMMITTED, ROWLOCK)
-    INNER JOIN CHI_TIET_PHIEU_MUA_SAM AS CTPMS WITH (READCOMMITTED, ROWLOCK)
-        ON PMS.MaPhieuMuaSam = CTPMS.MaPhieuMuaSam
-    INNER JOIN #DanhSachSanPham AS SP
-        ON CTPMS.MaSanPham = SP.MaSanPham
-    GROUP BY SP.MaSanPham;
+		CREATE TABLE #DanhSachSanPham (MaSanPham INT);
 
-    DROP TABLE #DanhSachSanPham;
+		-- Lấy danh sách sản phẩm đã bán
+		INSERT INTO #DanhSachSanPham (MaSanPham)
+		SELECT DISTINCT CTHD.MaSanPham
+		FROM CHI_TIET_HOA_DON AS CTHD WITH (READCOMMITTED, ROWLOCK)
+		INNER JOIN HOA_DON AS HD
+			ON CTHD.MaHoaDon = HD.MaHoaDon
+		INNER JOIN PHIEU_MUA_SAM AS PMS
+			ON HD.MaPhieuMuaSam = PMS.MaPhieuMuaSam
+		WHERE PMS.NgayDat = @Ngay;
+
+		-- Đếm số lượng khách hàng của từng sản phẩm
+		SELECT SP.MaSanPham, COUNT(DISTINCT PMS.MaKhachHang) AS SoLuongKhachHang
+		FROM PHIEU_MUA_SAM AS PMS WITH (READCOMMITTED, ROWLOCK)
+		INNER JOIN CHI_TIET_PHIEU_MUA_SAM AS CTPMS WITH (READCOMMITTED, ROWLOCK)
+			ON PMS.MaPhieuMuaSam = CTPMS.MaPhieuMuaSam
+		INNER JOIN #DanhSachSanPham AS SP
+			ON CTPMS.MaSanPham = SP.MaSanPham
+		GROUP BY SP.MaSanPham;
+
+		DROP TABLE #DanhSachSanPham;
+
+		COMMIT TRANSACTION
+	END TRY
+    BEGIN CATCH
+        ROLLBACK TRANSACTION;
+        THROW;
+    END CATCH;
 
 END;
 
@@ -67,22 +87,31 @@ CREATE OR ALTER PROCEDURE sp_LayDanhSachSanPhamDaBan
 AS
 BEGIN
 	set nocount on;
+	SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
 
-    -- Bảng tạm chứa danh sách hóa đơn
-    CREATE TABLE #DanhSachHoaDon (MaHoaDon INT, TongTien decimal(18,2));
+	BEGIN TRANSACTION;
+    BEGIN TRY
+		-- Bảng tạm chứa danh sách hóa đơn
+		CREATE TABLE #DanhSachHoaDon (MaHoaDon INT, TongTien decimal(18,2));
 
-    -- Lấy danh sách hóa đơn trong ngày
-    INSERT INTO #DanhSachHoaDon (MaHoaDon,TongTien)
-    EXEC sp_LayDanhSachHoaDonTrongNgay @Ngay;
+		-- Lấy danh sách hóa đơn trong ngày
+		INSERT INTO #DanhSachHoaDon (MaHoaDon,TongTien)
+		EXEC sp_LayDanhSachHoaDonTrongNgay @Ngay;
 
-    -- Bước 2: Lấy chi tiết hóa đơn để xác định số lượng sản phẩm đã bán
-    SELECT CTHD.MaSanPham, SUM(CTHD.SoLuong) AS SoLuong
-    FROM CHI_TIET_HOA_DON CTHD WITH (READCOMMITTED, ROWLOCK) -- Share Lock khi đọc chi tiết hóa đơn
-    INNER JOIN #DanhSachHoaDon HD
-        ON CTHD.MaHoaDon = HD.MaHoaDon
-    GROUP BY CTHD.MaSanPham;
+		-- Bước 2: Lấy chi tiết hóa đơn để xác định số lượng sản phẩm đã bán
+		SELECT CTHD.MaSanPham, SUM(CTHD.SoLuong) AS SoLuong
+		FROM CHI_TIET_HOA_DON CTHD WITH (READCOMMITTED, ROWLOCK) -- Share Lock khi đọc chi tiết hóa đơn
+		INNER JOIN #DanhSachHoaDon HD
+			ON CTHD.MaHoaDon = HD.MaHoaDon
+		GROUP BY CTHD.MaSanPham;
 
-	drop table #DanhSachHoaDon
+		drop table #DanhSachHoaDon
+	COMMIT TRANSACTION;
+    END TRY
+    BEGIN CATCH
+        ROLLBACK TRANSACTION;
+        THROW;
+    END CATCH;
 END
 GO
 
--- bo phan kinh doanh/TongDoanhThuTrongNgay.sql
+++ bo phan kinh doanh/TongDoanhThuTrongNgay.sql
@@ -6,30 +6,77 @@ AS
 BEGIN
 
     SET NOCOUNT ON;
-	 -- Initialize total revenue
-    SET @TongDThu = 0;
-
-    CREATE TABLE #DanhSachHoaDon (
+	SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
+/*
+	CREATE TABLE #DanhSachHoaDon (
         MaHoaDon INT,
         TongTien DECIMAL(18, 2)
     );
 
-    INSERT INTO #DanhSachHoaDon
+    -- Gọi procedure để lấy danh sách hoá đơn trong ngày
+    INSERT INTO #DanhSachHoaDon (MaHoaDon, TongTien)
     EXEC sp_LayDanhSachHoaDonTrongNgay @Ngay;
 
-    -- Calculate total revenue
-    SELECT @TongDThu = SUM(TongTien)
-    FROM #DanhSachHoaDon;
+    -- Khởi tạo biến tổng doanh thu
+    DECLARE @Tong MONEY = 0;
+    DECLARE @CurrentTongTien MONEY
 
-    -- Drop temporary table
+    -- Con trỏ để lặp qua từng hoá đơn trong danh sách
+    DECLARE HoaDon_Cursor CURSOR FOR
+    SELECT TongTien FROM #DanhSachHoaDon;
+    -- Mở con trỏ
+    OPEN HoaDon_Cursor;
+    -- Lấy dòng đầu tiên
+    FETCH NEXT FROM HoaDon_Cursor INTO @CurrentTongTien;
+    -- Vòng lặp tính tổng doanh thu
+    WHILE @@FETCH_STATUS = 0
+    BEGIN
+        SET @Tong = @Tong + @CurrentTongTien;
+        -- Lấy dòng tiếp theo
+        FETCH NEXT FROM HoaDon_Cursor INTO @CurrentTongTien;
+    END;
+    -- Đóng và giải phóng con trỏ
+    CLOSE HoaDon_Cursor;
+    DEALLOCATE HoaDon_Cursor;
+    -- Trả kết quả tổng doanh thu
+    SET @TongDThu = @Tong;
+    -- Xoá bảng tạm
     DROP TABLE #DanhSachHoaDon;
+	*/
+
+	 -- Initialize total revenue
+	BEGIN TRANSACTION;
+    BEGIN TRY
+		SET @TongDThu = 0;
+
+		CREATE TABLE #DanhSachHoaDon (
+			MaHoaDon INT,
+			TongTien DECIMAL(18, 2)
+		);
+
+		INSERT INTO #DanhSachHoaDon
+		EXEC sp_LayDanhSachHoaDonTrongNgay @Ngay;
+
+		-- Calculate total revenue
+		SELECT @TongDThu = SUM(TongTien)
+		FROM #DanhSachHoaDon;
+
+		-- Drop temporary table
+		DROP TABLE #DanhSachHoaDon;
 
     -- Return result
-    PRINT 'Total revenue of '+  CAST(@Ngay AS NVARCHAR(20))+' is ' + CAST(@TongDThu AS NVARCHAR(50));
+		PRINT 'Total revenue of '+  CAST(@Ngay AS NVARCHAR(20))+' is ' + CAST(@TongDThu AS NVARCHAR(50));
+
+		COMMIT TRANSACTION;
+    END TRY
+    BEGIN CATCH
+        ROLLBACK TRANSACTION;
+        THROW;
+    END CATCH;
 
 END
-GO
-DECLARE @TongDThu DECIMAL(18, 2);
+
+DECLARE @TongDThu DECIMAL(18,2);
 
 EXEC sp_TinhTongDoanhThuHangNgay @Ngay = '2024-11-01 14:00:00.000', @TongDThu = @TongDThu OUTPUT;
 
@@ -40,16 +87,27 @@ CREATE OR ALTER PROCEDURE sp_LayDanhSachHoaDonTrongNgay
 AS
 BEGIN
     SET NOCOUNT ON;
-	-- Lấy danh sách phiếu mua sắm có ngày đặt = @Ngay
-	 -- Lấy danh sách hóa đơn trong ngày
-    SELECT HD.MaHoaDon,hd.TongTien
-    FROM HOA_DON AS HD WITH (READCOMMITTED, ROWLOCK)
-    JOIN PHIEU_MUA_SAM AS PMS WITH (READCOMMITTED, ROWLOCK)
-        ON HD.MaPhieuMuaSam = PMS.MaPhieuMuaSam
-    WHERE PMS.NgayDat = @Ngay;
+	SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
+
+	BEGIN TRANSACTION;
+    BEGIN TRY
+		-- Lấy danh sách phiếu mua sắm có ngày đặt = @Ngay
+		 -- Lấy danh sách hóa đơn trong ngày
+		SELECT HD.MaHoaDon,hd.TongTien
+		FROM HOA_DON AS HD WITH (READCOMMITTED, ROWLOCK)
+		JOIN PHIEU_MUA_SAM AS PMS WITH (READCOMMITTED, ROWLOCK)
+			ON HD.MaPhieuMuaSam = PMS.MaPhieuMuaSam
+		WHERE PMS.NgayDat = @Ngay;
+
+		COMMIT TRANSACTION;
+    END TRY
+    BEGIN CATCH
+        ROLLBACK TRANSACTION;
+        THROW;
+    END CATCH;
 
 END
-GO
 
+--Vì nó đã lock luôn rồi nên lúc insert dô là ko cập nhật được => ko lấy ra được những hóa đơn sau khi đã dùng sp này
 exec sp_LayDanhSachHoaDonTrongNgay @Ngay = '2024-11-01 14:00:00.000'
 
